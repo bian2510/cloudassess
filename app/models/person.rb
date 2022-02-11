817eacd54a287dc0b9f290afae60319187dd43a8
Fabian Hernandez <fabian@covr.care>
2022-02-10 22:44:28 -0300

    fix query

--- app/models/person.rb
+++ app/models/person.rb
@@ -3,12 +3,12 @@ class Person < ApplicationRecord
 
   def self.search(search)
     if search
-      @people = Person.joins(:organisation)
-        .where('first_name like ?', "%'#{search}'%")
-        .or(Person.where('last_name like ?', "%'#{search}'%"))
+      Person.joins(:organisation)
+        .where('first_name like ?', "%#{search}%")
+        .or(Person.where('last_name like ?', "%#{search}%"))
         .or(Person.where("organisations.title like ?", "%#{search}%"))
     else
-      @people = Person.includes(:organisation).all
+      Person.includes(:organisation).all
     end
   end
 end
